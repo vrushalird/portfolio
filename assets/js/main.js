5b681599f5758fa296f1da9311acc778c26633f7
vrushalird <vrushali.geek@gmail.com>
2024-03-23 16:14:02 +0530

    portfolio first version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
 /**
-* Template Name: iPortfolio
-* Updated: Jan 29 2024 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+*   Main Javascript
+*   Author: Vrushali Daware
 */
+
 (function() {
   "use strict";
 
